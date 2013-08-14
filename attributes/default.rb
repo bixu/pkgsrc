21a4217b6819d035d82c01badadff774a1d5964f
Blake Irvin & Emily Nakashima <pair+blake+emily@wanelo.com>
2013-08-14 15:12:20 -0700

    support for configuring pkgsrc clients

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,8 @@
+default['pkgsrc']['repo_hosts'] = ["http://pkgsrc.joyent.com"]  # add other repo urls if you use your own pkgsrc mirror
+default['pkgsrc']['repo_path'] = "packages"
+
 default['pkgsrc']['upstream_url'] = "rsync://pkgsrc.joyent.com/pkgsrc-joyent/"
 default['pkgsrc']['local_mirror'] = "/opt/local/share/examples/nginx/html/pkgsrc-joyent/"
+default['pkgsrc']['platform'] = `head -n1 < /etc/release | awk '{print $1}'`.strip
+default['pkgsrc']['release'] = `grep release /etc/pkgsrc_version | awk '{print $2}'`.strip
+default['pkgsrc']['architecture'] = `grep architecture /etc/pkgsrc_version | awk '{print $2}'`.strip
