2dfa4801d0d7b3159ce3d0ea32cce1a87c91743a
Blake Irvin <blake.irvin@gmail.com>
2013-08-28 12:25:08 -0700

    Update pkgsrc db via pkgin list, not update (so it's one-time-only).

--- recipes/client.rb
+++ recipes/client.rb
@@ -4,7 +4,9 @@ template "/opt/local/etc/pkgin/repositories.conf" do
   action :nothing
 end.run_action(:create)
 
+# This will update the pkgsrc database if it has not already
+# been done
 execute "update pkgsrc database" do
-  command "pkgin -y -f update"
+  command "pkgin -y list"
   action :nothing
 end.run_action(:run)
