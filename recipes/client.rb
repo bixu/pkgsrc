f0d855793be7d922bd94829c4bfcdb8df869d58b
Nightmare Moon <wanelo@Nightmare-Moon.local>
2013-09-05 12:38:59 -0700

    improved notification handling for pkgsrc datbase update

--- recipes/client.rb
+++ recipes/client.rb
@@ -1,12 +1,10 @@
 template "/opt/local/etc/pkgin/repositories.conf" do
   source "repositories.conf.erb"
   mode 0755
-  action :nothing
-end.run_action(:create)
+  notifies :run, 'execute[update pkgsrc database]', :immediately
+end
 
-# This will update the pkgsrc database if it has not already
-# been done
 execute "update pkgsrc database" do
   command "pkgin -y list"
   action :nothing
-end.run_action(:run)
+end
