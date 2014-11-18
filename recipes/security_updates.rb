6558b7b8f7622b37a6719ce0c9ccebae54a0dab0
Blake Irvin <blake.irvin@yetu.com>
2014-11-18 12:28:23 +0100

    better pattern matching for package names

--- recipes/security_updates.rb
+++ recipes/security_updates.rb
@@ -3,6 +3,10 @@ execute "update vulnerability list" do
   command "pkg_admin fetch-pkg-vulnerabilities -u"
 end
 
-execute "install security udpates" do
-  command 'for package in $(pkg_admin audit | cut -d" " -f 2 | cut -d"-" -f 1 | uniq); do pkgin -y install $package; done'
+bash "install security udpates" do
+  code <<-EOH
+  for package in $(pkg_admin audit | cut -d" " -f2 | awk 'BEGIN{FS="-[0-9]."}{print $1}' | uniq)
+    do pkgin -y install $package
+  done
+  EOH
 end
