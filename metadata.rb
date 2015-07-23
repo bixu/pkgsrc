1c5894a1625082822d3644d42dcd043f97e13239
Eric Saxby <sax@livinginthepast.org>
2015-07-23 14:03:45 -0700

    0.5.2

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'bixu@wanelo.com, paul@wanelo.com'
 license          'MIT'
 description      'Installs/Configures pkgsrc'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.5.1'
+version          '0.5.2'
